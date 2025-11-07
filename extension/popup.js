5065b6314a3a0f84fe093bdd91b2268b49ef2ef3
Talha Ahmed <talha.codes970@gmail.com>
2025-11-07 16:29:16 +0530

    Added extension feature

--- extension/popup.js
+++ extension/popup.js
@@ -1,4 +1,4 @@
-const GEMINI_API_KEY = "AIzaSyAaKTwESBKSV8QzmEFKrAvzAOVafbWo0VQ";
+const GEMINI_API_KEY = "";
 const GEMINI_API_URL =
   "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent";
 let currentAnalysis = null;
